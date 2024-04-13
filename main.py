45c07ba6faaba75a3925cb36424ee180bfebd786
biondohod <biondohod@gmail.com>
2024-04-13 15:38:33 +0500

    use requests for itunes api

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-a = input("what's ur name? ")
-print(f"hello {a}")
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2: 
+    sys.exit()
+    
+response = requests.get("https://itunes.apple.com/search?entity=song&term=" + sys.argv[1])
+o = response.json()
+
+for result in o["results"]:
+    print(result["trackName"])
