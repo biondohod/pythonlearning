2dba2ce374c2a4eb07f60152476ab3594384e640
biondohod <biondohod@gmail.com>
2024-04-17 09:12:20 +0500

    regex

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-with open("names.txt")  as file:
-    for line in sorted(file):
-        print("hello, ",line.rstrip())
+import re 
+ 
+email = input("what's ur email? ").strip()
+# re.search - searching substring in the string
+# re.match - searching  substring in the string from the beggingin  
+# re.fullmatch - comparing 2 string
+if re.search(r"^(\w+\.)*\w+@(\w+\.)*\w+\.(edu|com)$", email, re.IGNORECASE):
+  print("valid")
+else:
+  print("invalid")
