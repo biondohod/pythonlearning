6c31442eef68c7af5c788dbbfebe2d99273b13b6
biondohod <biondohod@gmail.com>
2024-04-23 14:51:09 +0500

    classes

---
+++ student.py
@@ -0,0 +1,43 @@
+class Student: 
+  def __init__(self, name, house, patronus):
+    if not name:
+      raise ValueError("Missing name")
+    if house  not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+      raise ValueError("Invalid house")
+    self.name = name
+    self.house = house
+    self.patronus = patronus
+
+  def __str__(self):
+    return f"{self.name} from {self.house}"
+
+  def charm(self):
+    match self.patronus:
+      case  "Stag":
+        return "🦌"
+      case "Otter":
+        return "🦦"
+      case "Jack Russel terrier":
+        return "🐶"
+      case _:
+        return "🪄"
+
+  
+def main():
+  student = get_student()
+  print(student)
+  print("Expecto Patronum!")
+  print(student.charm())
+
+def get_student():
+  name = input("Name: ")
+  house = input("House: ")
+  patronus = input("Patronus: ")
+  student = Student(name, house, patronus)
+  try:
+    return student
+  except:
+    pass
+ 
+if __name__ == "__main__":
+  main()
